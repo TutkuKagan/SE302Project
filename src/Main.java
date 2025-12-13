7b9dde6b1a35fc20e7962ff0d1c791464ef1b5aa
AlidotEmre <aliemre3443@gmail.com>
2025-12-13 22:21:29 +0300

    Revert "Merge branch 'main' of https://github.com/TutkuKagan/SE302Project"
    This reverts commit d353a4cb02a1753b20c2d2af62f7aba0aea6ffeb, reversing
    changes made to 4f470900fdf5a189aa5744fe9cd1487264bfedf7.

--- src/Main.java
+++ src/Main.java
@@ -2,42 +2,21 @@ import java.io.IOException;
 import java.nio.file.Paths;
 
 public class Main {
-
     public static void main(String[] args) {
-
         DataRepository repo = new DataRepository();
-        CsvImportService importService = new CsvImportService(repo);
-
         try {
-
-            importService.importAll(
+            repo.loadAll(
                     Paths.get("sampleData_AllStudents.csv"),
                     Paths.get("sampleData_AllCourses.csv"),
                     Paths.get("sampleData_AllClassroomsAndTheirCapacities.csv"),
-                    Paths.get("sampleData_AllAttendanceLists.csv"),
-                    Paths.get("sampleData_slot_config.csv")
+                    Paths.get("sampleData_AllAttendanceLists.csv")
             );
-
-            System.out.println("Courses: " + repo.getCourses().size());
-            System.out.println("Students: " + repo.getStudents().size());
-            System.out.println("Classrooms: " + repo.getClassrooms().size());
-            System.out.println("Slots: " + repo.getSlots().size());
-
-
-            SchedulingEngine engine = new SchedulingEngine(repo);
-            Schedule schedule = engine.generateExamSchedule();
-
-
-            CsvExportService exportService = new CsvExportService(repo);
-            exportService.exportByCourse(schedule, Paths.get("schedule_by_course.csv"));
-            exportService.exportByRoom(schedule, Paths.get("schedule_by_room.csv"));
-            exportService.exportByStudent(schedule, Paths.get("schedule_by_student.csv"));
-            exportService.exportByDaySlot(schedule, Paths.get("schedule_by_day_slot.csv"));
-
-            System.out.println("Export completed.");
-
         } catch (IOException e) {
-            throw new RuntimeException("Error while importing/exporting CSV data", e);
+            throw new RuntimeException(e);
         }
+
+        System.out.println("Courses: " + repo.getCourses().size());
+        System.out.println("Students: " + repo.getStudents().size());
+        System.out.println("Classrooms: " + repo.getClassrooms().size());
     }
 }
